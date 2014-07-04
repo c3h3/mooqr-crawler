44c608234084b27a9266e482098b7d1756e67a0c
Chia-Chi Chang <c3h3.tw@gmail.com>
2014-07-04 14:56:10 +0800

    create script update_all_catalog_data.py / close #5

---
+++ bin/update_all_catalog_data.py
@@ -0,0 +1,13 @@
+'''
+Created on Jul 4, 2014
+
+@author: c3h3
+'''
+
+from mooqr_crawler.coursera.catalog_data import update_courses_db, update_instructors_db, update_session_db, update_universities_db
+
+update_courses_db() 
+update_instructors_db() 
+update_session_db() 
+update_universities_db()
+
